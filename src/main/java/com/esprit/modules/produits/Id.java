8f3b2bbf6f1ea6e16e0ec30715b372c351b3f988
ahmed <chaouch.ahmed@esprit.tn>
2025-03-22 03:43:05 +0100

    Ajout du module de gestion des produits dérivés

---
+++ src/main/java/com/esprit/modules/produits/Id.java
@@ -0,0 +1,4 @@
+package com.esprit.modules.produits;
+
+public @interface Id {
+}
---
+++ src/main/java/com/esprit/tests/mainProg.java
@@ -0,0 +1,10 @@
+package com.esprit.tests;
+
+public class mainProg {
+    public static void main(String[] args) {
+
+        Produit produit = new Produit("T-Shirt", 19.99);
+
+        produit.afficherDetails();
+    }
+}
